38e0f3660f2d9b4fe7ab3da882f5c30afbb112c0
Dominique Quatravaux <dominique@quatravaux.org>
2014-10-03 14:03:33 +0200

    simple calculation with 2 operands

--- calculator/calc.js
+++ calculator/calc.js
@@ -9,25 +9,50 @@ function compute(expr) {
     var calctype;
 
     try {
+        var resultat;
 
-        var resultat = eval(expr.toString()).toString();
 
-        if (expr.indexOf("+")) {
-            calctype="addition";
 
-        }
-        else if (expr.indexOf("-")) {
+        if (expr.indexOf("+")>=0) {
             calctype="addition";
+            var split = expr.split('+');
+            var operand1 = split[0];
+            var operand2 = split[1];
+            operand1=Number(operand1);
+            operand2=Number(operand2);
+            resultat=String(operand1+operand2);
         }
-        else if (expr.indexOf("/")) {
-            calctype="addition";
+        else if (expr.indexOf("-")>=0) {
+            calctype="soustraction";
+            var split = expr.split('-');
+            var operand1 = split[0];
+            var operand2 = split[1];
+            operand1=Number(operand1);
+            operand2=Number(operand2);
+            resultat=String(operand1-operand2);
         }
-        else if (expr.indexOf("*")) {
-            calctype="addition";
+        else if (expr.indexOf("/")>=0) {
+            calctype="division";
+            var split = expr.split('/');
+            var operand1 = split[0];
+            var operand2 = split[1];
+            operand1=Number(operand1);
+            operand2=Number(operand2);
+            resultat=String(operand1/operand2);
+        }
+        else if (expr.indexOf("*")>=0) {
+            calctype="multiplication";
+            var split = expr.split('*');
+            var operand1 = split[0];
+            var operand2 = split[1];
+            operand1=Number(operand1);
+            operand2=Number(operand2);
+            resultat=String(operand1*operand2);
         }
     } catch (e) {
         return "ERROR: " + String(e);
     }
+    return resultat;
 }
 
 function calculator (req, res) {
