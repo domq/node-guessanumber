37c9ba0f683917c0bdffb29a83000521d7950464
Dominique Quatravaux <dominique@quatravaux.org>
2014-10-17 14:00:33 +0200

    Refactoring with single_operation; add 3-operand case.

--- calculator/compute.js
+++ calculator/compute.js
@@ -1,49 +1,39 @@
-exports.compute = function compute(expr) {
-    var calctype;
+function split_operation(expr, operator){
+    var split = expr.split(operator);
+    var operand1 = Number(split[0]);
+    var operand2 = Number(split[1]);
+    return [operand1, operand2];
+}
 
+exports.single_operation = single_operation = function (op, number1, number2) {
+    switch(op) {
+        case "+":
+            return number1 + number2;
+        case "-":
+            return number1 - number2;
+        case "*" :
+            return number1 * number2;
+        case "/" :
+            return number1 / number2;
+        case "^":
+            return Math.pow(number1, number2);
+    }
+};
+
+exports.compute = function compute(expr) {
     try {
         var resultat;
 
+       ["+","-","*","/","^"].forEach(function (op) {
+           if (expr.indexOf(op)>=0) {
+               var operands = split_operation(expr, op);
+               resultat=single_operation(op, operands[0], operands[1]);
+           }
+       });
 
 
-        if (expr.indexOf("+")>=0) {
-            calctype="addition";
-            var split = expr.split('+');
-            var operand1 = split[0];
-            var operand2 = split[1];
-            operand1=Number(operand1);
-            operand2=Number(operand2);
-            resultat=String(operand1+operand2);
-        }
-        else if (expr.indexOf("-")>=0) {
-            calctype="soustraction";
-            var split = expr.split('-');
-            var operand1 = split[0];
-            var operand2 = split[1];
-            operand1=Number(operand1);
-            operand2=Number(operand2);
-            resultat=String(operand1-operand2);
-        }
-        else if (expr.indexOf("/")>=0) {
-            calctype="division";
-            var split = expr.split('/');
-            var operand1 = split[0];
-            var operand2 = split[1];
-            operand1=Number(operand1);
-            operand2=Number(operand2);
-            resultat=String(operand1/operand2);
-        }
-        else if (expr.indexOf("*")>=0) {
-            calctype="multiplication";
-            var split = expr.split('*');
-            var operand1 = split[0];
-            var operand2 = split[1];
-            operand1=Number(operand1);
-            operand2=Number(operand2);
-            resultat=String(operand1*operand2);
-        }
     } catch (e) {
         return "ERROR: " + String(e);
     }
-    return resultat;
+    return String(resultat);
 }
--- test/compute-test.js
+++ test/compute-test.js
@@ -3,7 +3,9 @@
  */
 
 var assert = require("assert");
-var compute = require("../calculator/compute").compute;
+var compute_module = require("../calculator/compute");
+var compute = compute_module.compute,
+    single_operation = compute_module.single_operation;
 
 describe("compute", function () {
    it("does basic additions", function () {
@@ -12,4 +14,22 @@ describe("compute", function () {
     it("does basic multiplications", function () {
         assert.equal(compute("2 * 3"), 6);
     });
+    it("does divisions", function () {
+        assert.equal(compute("5 / 2"), 2.5);
+    });
+    it("does power", function () {
+        assert.equal(compute("5 ^ 2"), 25);
+
+    });
+
+    it("does several additions", function () {
+        assert.equal(compute("1 + 2 + 3"), 6);
+
+    });
+});
+
+describe("single_operation", function () {
+   it("performs one operation with two operands", function () {
+      assert.equal(single_operation("+", 5, 7), 12);
+   });
 });
