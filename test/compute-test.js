9adfc9eb3ce0d5492564ca1829bf9dbeeded9858
Dominique Quatravaux <dominique@quatravaux.org>
2014-10-17 13:15:27 +0200

    Deleted old test files; temporarily disabled the Web test (pending refactoring)

--- test/compute-test.js
+++ test/compute-test.js
@@ -5,7 +5,7 @@
 var assert = require("assert");
 var compute = require("../calculator/compute").compute;
 
-describe.only("compute tests", function () {
+describe("compute", function () {
    it("does basic additions", function () {
        assert.equal(compute("2 + 3"), 5);
    });
--- test/guestanumber-test.js
+++ test/guestanumber-test.js
@@ -2,7 +2,8 @@ var assert = require('assert'),
     test = require('selenium-webdriver/testing'),
     webdriver = require('selenium-webdriver');
 
-test.describe('Google Search', function() {
+// Deactivated for now: the test really should fire its own Web server.
+test.xdescribe('Google Search', function() {
     test.it('should work', function() {
         var driver = new webdriver.Builder().withCapabilities(
             webdriver.Capabilities.chrome()).build();
